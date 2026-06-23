4b6dda5a50ec480a11a1f6b0b5338b85afccc84c
ashik-zainik-lab <ashik.200607.zan@gmail.com>
2026-06-23 13:47:18 +0600

    fixed

--- resources/views/sadmin/setting/partials/general-sidebar.blade.php
+++ resources/views/sadmin/setting/partials/general-sidebar.blade.php
@@ -34,4 +34,51 @@
             <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
         </a>
     </li>
+
+    <li>
+        <a href="{{ route('super-admin.setting.configuration-settings') }}"
+            class="d-flex justify-content-between align-items-center cg-10 {{ @$subConfigurationActiveClass }}">
+            <span class="fs-18 fw-600 lh-22 text-black">{{ __('Configuration Settings') }}</span>
+            <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{ route('super-admin.setting.currencies.index') }}"
+            class="d-flex justify-content-between align-items-center cg-10 {{ @$subCurrenciesActiveClass }}">
+            <span class="fs-18 fw-600 lh-22 text-black">{{ __('Currency Settings') }}</span>
+            <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{ route('super-admin.setting.gateway.index') }}"
+            class="d-flex justify-content-between align-items-center cg-10 {{ @$subGatewayActiveClass }}">
+            <span class="fs-18 fw-600 lh-22 text-black">{{ __('Payment Gateway') }}</span>
+            <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{ route('super-admin.setting.languages.index') }}"
+            class="d-flex justify-content-between align-items-center cg-10 {{ @$subLanguagesActiveClass }}">
+            <span class="fs-18 fw-600 lh-22 text-black">{{ __('Multi Language') }}</span>
+            <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('super-admin.setting.email-template') }}"
+            class="d-flex justify-content-between align-items-center cg-10 {{ @$subEmailActiveClass }}">
+            <span class="fs-18 fw-600 lh-22 text-black">{{ __('Email Template') }}</span>
+            <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{ route('super-admin.setting.notify-template') }}"
+            class="d-flex justify-content-between align-items-center cg-10 {{ @$subNotifyActiveClass }}">
+            <span class="fs-18 fw-600 lh-22 text-black">{{ __('Notification Template') }}</span>
+            <div class="d-flex text-textBlack"><i class="fa-solid fa-angle-right"></i></div>
+        </a>
+    </li>
 </ul>
